4527a0305f46cbea5bfc3e10440e8d71fa8570fe
Reteo <reteo.varala@gmail.com>
2016-05-17 00:42:51 -0400

    Rearranged the ores; rebalanced oregen.

--- CommonOres/main.js
+++ CommonOres/main.js
@@ -14,15 +14,16 @@ addCreativeTab("CommonOres.armor","Common Ores: Armor","CommonOres:armorSilverCh
 defineColor("RED", 255, 0, 0);
 defineColor("GREEN", 0, 255, 0);
 defineColor("BLUE", 0, 0,255);
-defineColor("BRONZE", 192, 88, 0);
+
+defineColor("ALUMINUM", 192, 192, 192);
 defineColor("COPPER", 192, 104, 64);
+defineColor("TIN", 128, 128, 128);
+defineColor("BRONZE", 192, 88, 0);
+defineColor("NICKEL", 221, 210, 168);
+defineColor("LEAD", 179, 182, 220);
+defineColor("SILVER", 227, 247, 255);
 defineColor("PLATINUM", 102, 160, 255);
 defineColor("ELECTRUM", 255, 255, 192);
-defineColor("TIN", 128, 128, 128);
-defineColor("ALUMINUM", 192, 192, 192);
-defineColor("SILVER", 227, 247, 255);
-defineColor("LEAD", 179, 182, 220);
-defineColor("NICKEL", 221, 210, 168);
 
 // Now for some generic stuff to build from.
 // NOTE: These are REQUIRED for addMetalBlock() and addMetalIngot() to work!
@@ -30,7 +31,7 @@ addBlock("blockMetalGeneric", "MetalBlock", 10.0, 10.0, "pickaxe", 1, "iron", "C
 addItem("ingotGeneric", "IngotGeneric", 64, "CommonOres.general");
 
 // Next, we add the blocks and ingots.
-var metals = ["bronze","electrum","silver","copper","tin", "platinum","aluminum","lead","nickel"];
+var metals = ["aluminum","copper","tin","bronze","nickel","lead","silver","platinum","electrum"];
 
 for(var m in metals){
     addMetalBlock(metals[m]);
@@ -40,7 +41,7 @@ for(var m in metals){
 }
 
 // Then, we add the ores.
-var ores = ["Silver","Copper","Tin", "Platinum","Aluminum","Lead","Nickel"];
+var ores = ["Aluminum","Copper","Tin","Nickel","Lead","Silver","Platinum"];
 
 for(var m in ores){
     addBlock("ore" + ores[m], "CoreBlock", 3.0, 5.0, "pickaxe", 1, "rock", "CommonOres.general");
@@ -51,29 +52,29 @@ for(var m in ores){
 // Now, we move onto materials.
 // First, for tools.
 addToolMaterial( "FLINT", 1, 175, 5.0,1.0,10, "itemFlint" );
-addToolMaterial( "COPPER", 2, 180, 5.0,1.0, 5, "ingotCopper" );
-addToolMaterial( "ELECTRUM", 1, 100,20.0,0.0,30, "ingotElectrum" );
-addToolMaterial( "SILVER", 2, 25,16.0,1.0,20, "ingotSilver" );
 addToolMaterial( "ALUMINUM", 2, 175, 4.5,2.0, 4, "ingotAluminum" );
+addToolMaterial( "COPPER", 2, 180, 5.0,1.0, 5, "ingotCopper" );
 addToolMaterial( "BRONZE", 2, 250, 6.0,1.0,14, "ingotBronze" );
-addToolMaterial( "PLATINUM", 2,400, 7.0,1.0,30, "ingotPlatinum" );
 addToolMaterial( "NICKEL", 2, 200, 7.0,2.5,17, "ingotNickel" );
 addToolMaterial( "LEAD", 0, 131, 7.0,4.0,0, "ingotLead" );
+addToolMaterial( "SILVER", 2, 25,16.0,1.0,20, "ingotSilver" );
+addToolMaterial( "PLATINUM", 2,400, 7.0,1.0,30, "ingotPlatinum" );
+addToolMaterial( "ELECTRUM", 1, 100,20.0,0.0,30, "ingotElectrum" );
 
 // Then, for armor.
+addArmorMaterial( "ALUMINUM", 6,2,4,3,1,20,"ingotAluminum" );
 addArmorMaterial( "COPPER", 8,2,5,4,1, 9,"ingotCopper" );
 addArmorMaterial( "TIN", 8,2,4,3,1,15,"ingotTin" );
-addArmorMaterial( "ALUMINUM", 6,2,4,3,1,20,"ingotAluminum" );
 addArmorMaterial( "BRONZE", 18,2,6,5,2, 9,"ingotBronze" );
-addArmorMaterial( "SILVER", 7,2,5,3,1,20,"ingotSilver" );
-addArmorMaterial( "ELECTRUM", 8,2,5,4,1,28,"ingotElectrum" ); 
-addArmorMaterial( "PLATINUM", 28,3,8,6,3,20,"ingotPlatinum" );
 addArmorMaterial( "NICKEL", 14,2,6,5,2, 15,"ingotNickel" );
+addArmorMaterial( "SILVER", 7,2,5,3,1,20,"ingotSilver" );
+addArmorMaterial( "PLATINUM", 28,3,8,6,3,20,"ingotPlatinum" );
+addArmorMaterial( "ELECTRUM", 8,2,5,4,1,28,"ingotElectrum" ); 
 
 // Now, to actually make the tools and armor.
 // First, the tools.
 var toolTypes = ["Sword","Hoe","Pickaxe","Axe","Shovel"];
-var toolMats = ["Flint","Bronze","Electrum","Silver","Copper","Platinum","Aluminum","Nickel"];
+var toolMats = ["Flint","Aluminum","Copper","Bronze","Nickel","Silver","Platinum","Electrum"];
 
 for(var m in toolMats) for(var n in toolTypes){
 	addItem("tool" + toolTypes[n] + toolMats[m],"Tool" + toolTypes[n],1,"CommonOres.tools").tooldata(ToolData(toolMats[m].toUpperCase()));
@@ -91,7 +92,7 @@ addShapedRecipe("CommonOres:toolPipeLead", [
 
 // Next, the Armor
 var armorTypes = ["Helmet","Chestplate","Leggings","Boots"];
-var armorMats = ["Bronze","Electrum","Silver","Copper","Platinum","Aluminum","Nickel"];
+var armorMats = ["Aluminum","Copper","Bronze","Nickel","Silver","Platinum","Electrum"];
 
 for(var m in armorMats) for(var n in armorTypes) {
 	addItem("armor" + armorMats[m] + armorTypes[n],"CoreArmor",1,"CommonOres.armor").armordata(ArmorData(armorMats[m].toUpperCase(),armorTypes[n].toLowerCase()));
@@ -101,13 +102,13 @@ for(var m in armorMats) for(var n in armorTypes) {
 // Finally, the ore needs to be spawned in the world.
 // The order is "name", vein size, chances per chunk, lower Y level, higher Y level.
 ores = [
-    ["Silver", 8, 4, 8, 40],
-    ["Copper", 7, 24, 5, 75],
-    ["Tin", 7, 22, 6, 60],
-    ["Alumninum", 7, 18, 5, 45],
-    ["Platinum", 4, 2, 6, 16],
-    ["Lead", 8, 8, 11, 35],
-    ["Nickel", 8, 6, 16, 48]];
+    ["Alumninum", 7, 24, 5, 64],
+    ["Copper", 7, 22, 5, 64],
+    ["Tin", 7, 20, 5, 64],
+    ["Nickel", 8, 12, 16, 48],
+    ["Lead", 8, 8, 8, 32],
+    ["Silver", 8, 4, 8, 32],
+    ["Platinum", 4, 2, 5, 16]];
 
 for (var m in ores){
     addOreGeneration().blockToGenerate("CommonOres:ore"+ores[m][0]).veinSize(ores[m][1]).chancesPerChunk(ores[m][2]).startY(ores[m][3]).endY(ores[m][4]);
