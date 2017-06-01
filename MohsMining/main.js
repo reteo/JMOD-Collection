ef734b5ec3b2c89e2a30d276edef4490cf6100d0
Lampros Liontos <reteo.varala@gmail.com>
2017-06-01 10:22:31 -0400

    Changed comments, updated code to reflect changes in JMOD.

--- MohsMining/main.js
+++ MohsMining/main.js
@@ -1,5 +1,10 @@
-/* Moh's Mining is a mod that provides common materials used by many mods.
- * This mod includes weapons, tools, and armor.
+/* Moh's Mining is a mod that rearranges mining progression in order of mineral hardness.
+ * It also changes the hardness and blast resistence to match realistic Moh's hardness
+ * values and specific weights (density).  Additionally, several more materials are added
+ * to the mining progression, and a couple of alloys are made available.
+ *
+ * This mod includes weapons, tools, and armor, also adjusted to match hardness and density
+ * of real materials.
  * 
  * It is recommended to install Vanilla Ingot Works in order to make the alloys.
  */
@@ -27,10 +32,10 @@ addCreativeTab("MohsMining.tools","Moh's Mining: Tools and Weapons","MohsMining:
 addCreativeTab("MohsMining.armor","Moh's Mining: Armor","MohsMining:armorSilverChestplate");
 
 // Creating tool and armor material stats.
-load("materials.js");
+loadjs("materials.js");
 
 // Creating items and blocks.
-load("objects.js");
+loadjs("objects.js");
 
 // Creating and distributing ores.
-load("ores.js");
+loadjs("ores.js");
